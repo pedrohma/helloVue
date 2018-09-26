e8e4731523426a7f957f80a4826f00d0eb0a06ad
Pedro Henrique de Miranda Arthur <pedrohma95@gmail.com>
2018-09-26 12:30:20 -0700

    adding more

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,37 @@
-var app = new Vue({ 
+var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!'
     }
 });
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        items: [
+            { text: 'Bread' },
+            { text: 'Brocolli' },
+            { text: 'Butter' }
+        ]
+    },
+    methods:{
+        addList: function() {
+            this.items.push({ text: this.item });
+            this.item = "";
+        }
+    }
+})
