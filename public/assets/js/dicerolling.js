6b79d4a11833faf4081727e3a8d89671edfe5ed7
Pedro Henrique de Miranda Arthur <pedrohma95@gmail.com>
2018-10-02 11:31:41 -0700

    2

--- public/assets/js/dicerolling.js
+++ public/assets/js/dicerolling.js
@@ -1,12 +1,15 @@
 var dice = new Vue({
     el: '#diceValue',
     data: {
-        value: ''
+        dice1: '',
+        dice2: ''
     },
     methods:{
         roll: function(){
             var num = Math.floor(Math.random() * 6) + 1;
-            dice.value = num;
+            var num2 = Math.floor(Math.random() * 6) + 1;
+            dice.dice1 = num;
+            dice.dice2 = num2;
         }
     }
 })
